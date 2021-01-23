50b7a3e654436b00a3bfba7fc5c20f31b0438d29
Daniel <daniel@192.168.1.9>
2021-01-23 11:42:21 +0300

    colors and logo

--- lib/theme/theme_colors.dart
+++ lib/theme/theme_colors.dart
@@ -8,10 +8,10 @@ Color background2 = Colors.grey[800];
 
 Color background3 = Colors.grey[900];
 
-Color dark = Color(0Xff9d8189);
+Color dark = Color(0Xff2e520d);
 
-Color light = Color(0x45c461);
+Color light = Color(0xff73ed00);
 
-Color primary = Color(0x135c23);
+Color primary = Color(0xff88c251);
 
-Color accent = Color(0x057a1f);
+Color accent = Color(0xff544414);
